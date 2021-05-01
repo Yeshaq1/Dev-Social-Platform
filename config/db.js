ab78188d3335df16bc5aca1f3fa1a9a7da49cabe
Yousef <yeshaq@realtermenergy.com>
2021-05-01 14:55:52 -0400

    second commit with DB and config setup

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
+
+
+async function connectDB(){
+    try{
+        await mongoose.connect(db,{useNewUrlParser:true, useUnifiedTopology: true} );
+        console.log("MongoDB is connected...")
+    }
+
+    catch(err){
+        console.log(err.message);
+        //Exit process by force
+        process.exit(1);
+    }
+
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 const express = require('express');
-
+const connectDB = require("./config/db");
 const app = express();
-
 const PORT = process.env.PORT || 5000;
 
-
+connectDB();
 
 app.get("/", (req,res)=>{
 
     res.send("hello there")
 });
-
 
 
 app.listen(PORT, ()=>{
