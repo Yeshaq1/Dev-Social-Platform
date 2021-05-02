d4fb72bd9a6e5e05e182e006866a06ce15ebf50e
Yousef <yeshaq@realtermenergy.com>
2021-05-01 20:04:43 -0400

    Version 4, updated the express validator and post user route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,24 @@
 const express = require("express");
-const router = express.Router()
+const router = express.Router();
+const {body, validationResult} = require("express-validator");
 
 // @route     POST api/users
 // @desc      TEST route
 // @access    Private 
 
-router.post("/", (req,res)=>{
+router.post("/", 
+
+[body('name','name is required').not().isEmpty(),
+ body('email',"please include a valid email").isEmail(),
+ body('password','please enter a password with 6 or more characters').isLength({min:6})
+] 
+
+,(req,res)=>{
+const errors = validationResult(req);
+if(!errors.isEmpty()){
+    return res.status(400).json({errors:errors.array()});
+}
+
 console.log(req.body);
 res.send("user route");
 });
