c5b48b1e750831b3297458e9a94da714d561608e
Yousef <yeshaq@realtermenergy.com>
2021-05-03 17:57:18 -0400

    Get profile/me to return the logged in user's profile model

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,12 +1,33 @@
 const express = require("express");
-const router = express.Router()
+const router = express.Router();
+const auth = require("../../middleware/auth");
+const Profile = require("../../models/Profile");
+const User = require("../../models/Users");
 
-// @route     GET api/profile
-// @desc      TEST route
+// @route     GET api/profile/me
+// @desc      Get current users profile route
 // @access    Private 
 
-router.get("/", (req,res)=>{
-res.send("user profile");
+router.get("/me", auth, async (req,res)=>{
+
+try {
+
+    const profile = await Profile.findOne({user: req.user.id}).populate('user',['name','avatar']);
+
+    if (!profile){
+
+        return res.status(400).json({msg:"There is no profile for this user"});
+    }
+
+    res.json(profile);
+
+}
+
+catch(err){
+    console.error(err.message);
+    res.status(500).send("Sever Error");
+}
+
 });
 
 module.exports = router;
