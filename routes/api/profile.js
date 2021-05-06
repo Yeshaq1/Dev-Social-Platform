475d723df3ec1ee0fe61313561e28cfb9e0d38be
Yousef <yeshaq@realtermenergy.com>
2021-05-05 22:13:35 -0400

    added delete and create functionality for the experienece section of the profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -172,8 +172,8 @@ router.get("/user/:user_id", async (req, res)=>{
 
 });
 
-// @route     DELETE api/profile/me
-// @desc      DELETE specific profile
+// @route     DELETE api/profile/
+// @desc      DELETE specific profile and User
 // @access    Private 
 
 router.delete("/", auth, async (req,res)=>{
@@ -190,14 +190,91 @@ try {
    
  await Profile.deleteOne({user: req.user.id});
  await User.deleteOne({_id: req.user.id});
- 
+
  res.send("Profile Deleted");
 
 } catch (err) {
      console.error(err.message);
      return res.status(500).send("Server Error")
 }
+
+});
     
+
+
+// @route     PUT api/profile/experience
+// @desc      PUT a specific experience 
+// @access    Private 
+
+router.put("/experience", [auth, [ 
+
+body('title','title is required').not().isEmpty(),
+body('company',"company name is required").not().isEmpty(),
+body('from','from is required')]],
+
+async (req,res)=>{
+
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+    return res.status(400).json({errors:errors.array()});
+    }
+
+    const {
+        title, 
+        from,
+        current,
+        to,
+        company,
+        location,
+        description
+    } = req.body;
+
+    const newExp = {
+        title,
+        from,
+        current,
+        to,
+        company,
+        location,
+        description
+    };
+
+    try {
+        
+        const profile = await Profile.findOne({user:req.user.id});
+        
+        profile.experience.unshift(newExp);
+        await profile.save();
+        res.json(profile);
+
+    } catch (err) {
+        console.error(err.message);
+        return res.status(500).send("Server Error");
+    }
+ 
+
+});
+
+
+// @route     Delete api/profile/experience/experience_id
+// @desc      Delete a specific experience 
+// @access    Private 
+
+router.delete("/experience/:experience_id", auth, async (req,res)=>{
+
+  try {
+    
+    profile = await Profile.findOneAndUpdate({user:req.user.id},
+        {$pull:{experience:{_id: req.params.experience_id}}},
+        {new :true});
+
+    res.json(profile);
+      
+  } catch (err) {
+      console.error(err.message);
+      return res.status(500).send("Server Error");
+  }
+
 });
 
 module.exports = router;
