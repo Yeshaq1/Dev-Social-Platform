e12c834d2a2c5a7bebd0ada7724cf0ec7aab33b1
Yousef <yeshaq@realtermenergy.com>
2021-05-14 19:59:29 -0400

    added reducers, and actions

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,22 +5,28 @@ import Navbar from './components/layout/Navbar';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import Register from './components/Auth/Register';
 import Login from './components/Auth/Login';
+//Redux
+
+import { Provider } from 'react-redux';
+import store from './store';
 
 function App() {
   return (
-    <Router>
-      <Fragment>
-        <Navbar />
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
 
-        <Route exact path='/' component={Landing} />
-        <section className='container'>
-          <Switch>
-            <Route exact path='/register' component={Register} />
-            <Route exact path='/login' component={Login} />
-          </Switch>
-        </section>
-      </Fragment>
-    </Router>
+          <Route exact path='/' component={Landing} />
+          <section className='container'>
+            <Switch>
+              <Route exact path='/register' component={Register} />
+              <Route exact path='/login' component={Login} />
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
   );
 }
 
